b89d9e41dfc9e6d0b066195eccbf335670391d18
Abdul Hajiyev <abdulhajiyev@gmail.com>
2021-10-15 21:47:25 +0400

    Add project files.

---
+++ MediatorPattern/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+
+namespace MediatorPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Button button = new ();
+            Textbox textbox = new ();
+            Checkbox checkbox = new ();
+            new AuthenticationDialog(button, textbox, checkbox);
+
+            Console.WriteLine("Client triggets operation Button.");
+            button.Click();
+
+            Console.WriteLine();
+
+            Console.WriteLine("Client triggers operation Textbox.");
+            textbox.Textchanged();
+
+            Console.WriteLine();
+
+            Console.WriteLine("Client triggers operation Checkbox.");
+            checkbox.Checked();
+
+        }
+    }
+
+    public interface IMediator
+    {
+        void Notify(object sender, string ev);
+    }
+
+
+    // Concrete mediator
+    class AuthenticationDialog : IMediator
+    {
+        private readonly Button _button;
+        private readonly Textbox _textbox;
+        private readonly Checkbox _checkbox;
+
+        public AuthenticationDialog(Button button, Textbox textbox, Checkbox checkbox)
+        {
+            _button = button;
+            _button.SetMediator(this);
+            _textbox = textbox;
+            _textbox.SetMediator(this);
+            _checkbox = checkbox;
+            _checkbox.SetMediator(this);
+        }
+
+        public void Notify(object sender, string ev)
+        {
+            switch (ev)
+            {
+                case "Click":
+                    Console.WriteLine("Mediator reacts on Button and triggers folowing operations:");
+                    _button.Click();
+                    break;
+                case "Textbox":
+                    Console.WriteLine("Mediator reacts on Textbox and triggers following operations:");
+                    _textbox.Textchanged();
+                    break;
+                case "Checkbox":
+                    Console.WriteLine("Mediator reacts on Checkbox and triggers following operations:");
+                    _checkbox.Checked();
+                    break;
+            }
+        }
+    }
+
+
+    class BaseComponent
+    {
+        protected IMediator Mediator;
+
+        protected BaseComponent(IMediator mediator = null)
+        {
+            Mediator = mediator;
+        }
+
+        public void SetMediator(IMediator mediator)
+        {
+            Mediator = mediator;
+        }
+    }
+
+    class Button : BaseComponent
+    {
+        public void Click()
+        {
+            Console.WriteLine("Button clicked");
+            Mediator.Notify(this, "click");
+        }
+    }
+
+    class Textbox : BaseComponent
+    {
+        public void Textchanged()
+        {
+            Console.WriteLine("Txt changed");
+            Mediator.Notify(this, "text");
+        }
+    }
+
+    class Checkbox : BaseComponent 
+    {
+        public void Checked()
+        {
+            Console.WriteLine("Cbx checked");
+            Mediator.Notify(this, "checked");
+        }
+    }
+}
---
+++ MementoPattern/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Threading;
+
+namespace MementoPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Completed level 1
+            Player player = new Player();
+            player.Level = 1;
+            player.Score = 100;
+            player.Health = "100%";
+            Console.WriteLine("----------- Player info after completing level 1 ---------------------");
+            player.DisplayPlayerInfo();
+            // when player completes any level then create checkpoint for that level.
+            CareTaker careTaker = new CareTaker();
+            careTaker.LevelMarker = player.CreateMarker(player);
+
+            // Delay
+            Thread.Sleep(2000);
+
+            player.Level = 2;
+            player.Score = 130;
+            player.Health = "80%";
+            Console.WriteLine("--------------- Player info in level 2 --------------------------------");
+            player.DisplayPlayerInfo();
+
+            // if players loses all the lifeline then restore the game from level 1
+            player.RestoreLevel(careTaker.LevelMarker);
+            Console.WriteLine("------------- Player info after restoring level 1 data ----------------");
+            player.DisplayPlayerInfo();
+            Console.ReadLine();
+        }
+    }
+
+    // Memento class
+    public class Memento
+    {
+        public int Level;
+        public int Score;
+        public string Health;
+
+        public Memento(int level, int score, string health)
+        {
+            Level = level;
+            Score = score;
+            Health = health;
+        }
+    }
+
+    // CareTaker class
+    public class CareTaker
+    {
+        public Memento LevelMarker;
+    }
+
+    // Originator class
+    public class Player
+    {
+        public int Level;
+        public int Score;
+        public string Health;
+        public int Lifeline = 3;
+
+        public Memento CreateMarker(Player player)
+        {
+            return new Memento(player.Level, player.Score, player.Health);
+        }
+
+        public void RestoreLevel(Memento playerMemento)
+        {
+            Level = playerMemento.Level;
+            Score = playerMemento.Score;
+            Health = playerMemento.Health;
+            Lifeline -= 1;
+        }
+
+        public void DisplayPlayerInfo()
+        {
+            Console.WriteLine("Level: " + Level);
+            Console.WriteLine("Score: " + Score);
+            Console.WriteLine("Health: " + Health);
+            Console.WriteLine("Lifeline left: " + Lifeline);
+        }
+    }
+}
